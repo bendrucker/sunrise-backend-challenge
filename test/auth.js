9e4d1cde9f23b55d22fa1c2cfa85261284f1fa06
Ben Drucker <bvdrucker@gmail.com>
2015-05-30 20:22:35 -0400

    Add tests for use of query string accessToken

--- test/auth.js
+++ test/auth.js
@@ -1,25 +1,59 @@
 'use strict'
 
-var expect = require('code').expect
-var Lab = require('lab')
-var lab = exports.lab = Lab.script()
+var test = require('tape')
 var compose = require('../')
 
-lab.experiment('auth', function () {
+test('auth', function (t) {
   var server
-  lab.before(function (done) {
+  t.test('setup', function (t) {
     compose(function (err, _server_) {
-      if (err) return done(err)
+      if (err) return t.end(err)
       server = _server_
-      done()
+      t.end()
     })
   })
 
-  lab.test('redirects to google', function (done) {
+  t.test('redirects to google', function (t) {
+    t.plan(2)
     server.inject('/authenticate', function (response) {
-      expect(response.statusCode).to.equal(302)
-      expect(response.headers.location).to.contain('google.com')
-      done()
+      t.equal(response.statusCode, 302, 'status code')
+      t.ok(/google\.com/.test(response.headers.location), 'location')
     })
   })
+
+  t.test('enforces accessToken in query', function (t) {
+    t.plan(2)
+    server.route({
+      path: '/unauthed',
+      method: 'get',
+      handler: function (request, reply) {
+        reply('Success')
+      }
+    })
+    server.inject('/unauthed', function (response) {
+      t.equal(response.statusCode, 401, 'status code')
+      t.ok(/Missing accessToken/.test(response.result.message), 'message')
+    })
+  })
+
+  t.test('exposes access token as "token" credential', function (t) {
+    t.plan(2)
+    server.route({
+      path: '/authed',
+      method: 'get',
+      handler: function (request, reply) {
+        reply(request.auth.credentials.token)
+      }
+    })
+    server.inject('/authed?accessToken=theToken', function (response) {
+      t.equal(response.statusCode, 200)
+      t.equal(response.result, 'theToken')
+    })
+  })
+
+  t.test('after', function (t) {
+    server.stop(t.end.bind(t))
+  })
+
+  t.end()
 })
