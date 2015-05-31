26c95c3049e91f10881573ea53c8959686218447
Ben Drucker <bvdrucker@gmail.com>
2015-05-30 20:00:31 -0400

    Implement request plugin for formatting HTTP calls to API

---
+++ server/request.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var wreck = require('wreck')
+var qs = require('qs')
+var url = require('url')
+var apiBase = 'https://www.googleapis.com/calendar/v3/'
+
+exports = module.exports = function (server, options, next) {
+  server.method('request', request)
+  next()
+}
+exports.attributes = {
+  name: 'request'
+}
+
+function request (method, path, token, next) {
+  var uri = url.resolve(apiBase, path)
+  var headers = {Authorization: 'Bearer ' + token}
+  wreck[method](uri, {headers: headers, json: true}, function (err, response, payload) {
+    next(err, payload)
+  })
+}
