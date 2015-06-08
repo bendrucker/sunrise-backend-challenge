1380f17715322d6d9a19783c774af2881c220ea6
Ben Drucker <bvdrucker@gmail.com>
2015-06-08 13:31:26 -0400

    Add oauth routes

---
+++ auth/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var qs = require('qs')
+var url = require('url')
+var path = require('path')
+var token = require('./token')
+
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    var callbackUrl = url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: path.join(req.path, 'callback')
+    })
+    res.redirect('https://accounts.google.com/o/oauth2/auth?' + qs.stringify({
+      redirect_uri: callbackUrl,
+      response_type: 'code',
+      client_id: app.locals.google.clientId,
+      scope: 'https://www.googleapis.com/auth/calendar.readonly'
+    }))
+  })
+
+  app.get('/callback', function (req, res) {
+    var code = req.query.code
+    token.get(code, app.locals.google, function (err, auth, response) {
+      if (err) {
+        res.status(response.statusCode).json({
+          error: err.message
+        })
+      } else {
+        res.json(auth)
+      }
+    })
+  })
+}
---
+++ auth/routes.spec.js
@@ -0,0 +1,86 @@
+'use strict'
+
+var test = require('tape')
+var url = require('url')
+var express = require('express')
+var shot = require('shot')
+var http = require('http')
+var url = require('url')
+var readAllStream = require('read-all-stream')
+var proxyquire = require('proxyquire')
+var token = {}
+
+var routes = proxyquire('./routes', {
+  './token': token
+})
+
+test('auth routes', function (t) {
+  var app = express()
+  var auth = express()
+  auth.locals = {
+    google: {
+      clientId: 'theClientId',
+      clientSecret: 'theClientSecret'
+    }
+  }
+  routes(auth)
+  app.use('/authenticate', auth)
+
+  var server = app.listen(9999, function (err) {
+    if (err) return t.end(err)
+
+    t.test('redirect', function (t) {
+      t.plan(3)
+      http.get('http://localhost:9999/authenticate', function (response) {
+        t.equal(response.statusCode, 302)
+        t.ok(response.headers.location)
+        var query = url.parse(response.headers.location, true).query
+        t.deepEqual(query, {
+          client_id: 'theClientId',
+          redirect_uri: 'http://localhost:9999/callback',
+          response_type: 'code',
+          scope: 'https://www.googleapis.com/auth/calendar.readonly'
+        })
+      })
+      .on('error', t.end)
+    })
+
+    t.test('callback', function (t) {
+      t.plan(4)
+      // mock token.get
+      token.get = function (code, client, callback) {
+        t.equal(code, 'theCode', 'auth code')
+        t.deepEqual(client, auth.locals.google, 'receives client')
+        callback(null, {access_token: 'at'}, {statusCode: 200})
+      }
+      http.get('http://localhost:9999/authenticate/callback?code=theCode', function (response) {
+        t.equal(response.statusCode, 200, '200')
+        readAllStream(response, function (err, data) {
+          if (err) return t.end(err)
+          data = JSON.parse(data)
+          t.deepEqual(data, {access_token: 'at'})
+        })
+      })
+    })
+
+    t.test('callback - err', function (t) {
+      t.plan(2)
+      // mock token.get to fail
+      token.get = function (code, client, callback) {
+        callback(new Error('broken'), {}, {statusCode: 401})
+      }
+      http.get('http://localhost:9999/authenticate/callback', function (response) {
+        t.equal(response.statusCode, 401, 'copies google err code')
+        readAllStream(response, function (err, data) {
+          if (err) return t.end(err)
+          data = JSON.parse(data)
+          t.deepEqual(data, {error: 'broken'})
+        })
+      })
+    })
+
+    t.test(function (t) {
+      server.close(t.end)
+    })
+  })
+})
