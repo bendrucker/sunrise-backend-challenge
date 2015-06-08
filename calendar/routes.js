9aa3386afd49a2cd0eaaa842c4257c2d64709eae
Ben Drucker <bvdrucker@gmail.com>
2015-06-08 13:40:16 -0400

    Add generic err handling to routes

--- calendar/routes.js
+++ calendar/routes.js
@@ -4,6 +4,14 @@ var calendars = require('./api')
 
 module.exports = function (app) {
   app.get('/calendars/:id', function (req, res) {
-    calendars.list(res.locals.token, res.json)
+    calendars.list(res.locals.token, function (err, calendars) {
+      if (err) {
+        res.status(500).json({
+          error: 'Could not fetch calendars'
+        }) 
+      } else {
+        res.json(calendars)
+      }
+    })
   })
 }
--- event/routes.js
+++ event/routes.js
@@ -4,6 +4,14 @@ var calendars = require('./api')
 
 module.exports = function (app) {
   app.get('/calendars/:id/events', function (req, res) {
-    events.list(req.params.id, res.locals.token, res.json)
+    events.list(req.params.id, res.locals.token, function (err, events) {
+      if (err) {
+        res.status(500).json({
+          error: 'Could not fetch events'
+        }) 
+      } else {
+        res.json(events)
+      }
+    })
   })
 }
